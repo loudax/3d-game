26356b95c6f1630386bb7ec2c1e9a70f453d5817
loudax <liuxinyi425@126.com>
2020-09-23 16:51:15 +0800

    first commit

---
+++ tictactoe/Assets/test.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    void Awake () {
+        Debug.Log("Awake");
+    }
+ 
+    void Start () {
+        Debug.Log("Start");
+	}
+	
+    void Update () {
+        Debug.Log("Update");
+	}
+ 
+    void FixedUpdate () {
+        Debug.Log("FixedUpdate");
+    }
+
+    void LateUpdate () {
+        Debug.Log("LateUpdate");
+    }
+ 
+    void OnGUI () {
+        Debug.Log("OnGUI");
+    }
+ 
+    void OnDisable () {
+        Debug.Log("OnDisable");
+    }
+ 
+    void OnEnable () {
+        Debug.Log("OnEnable");
+    }
+}
