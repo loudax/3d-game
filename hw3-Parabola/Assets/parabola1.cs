1994cb8b47ee0563be5e9bf37e5bd6599d1169a4
loudax <liuxinyi425@126.com>
2020-10-05 12:40:52 +0800

    hw3

---
+++ hw3-Parabola/Assets/parabola1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parabola1 : MonoBehaviour
+{
+    public float speed = 1; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += Vector3.right * Time.deltaTime * 5;
+        this.transform.position += Vector3.down * Time.deltaTime * (speed / 10);
+        speed++;         
+    }
+}
---
+++ hw3-Parabola/Assets/parabola2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parabola2 : MonoBehaviour
+{
+    public float speed = 1; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = new Vector3(Time.deltaTime * 5, - Time.deltaTime * (speed / 10), 0);
+        this.transform.position += v;
+        speed++;    
+    }
+}
