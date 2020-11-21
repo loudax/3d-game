a298419886725c61d91f7fbe9df5499d5ef0c42d
loudax <liuxinyi425@126.com>
2020-11-21 17:16:45 +0800

    hw8

---
+++ hw8-ParticleSystem2/Assets/changeColor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    private ParticleSystem particleSys;
+    private bool colorA;
+    private int time;
+
+    void Start()
+    {
+        particleSys = this.GetComponent<ParticleSystem>();
+        colorA = true;
+        time = 0;
+    }
+
+    void Update()
+    {
+        changecolor();
+    }
+
+    void OnGUI()
+	{
+        if(Input.GetMouseButtonDown(0)){
+			time++;
+            if(time%2 == 0){
+                colorA = !colorA;
+            }
+		}
+	}
+
+    void changecolor()
+    {
+        var main = particleSys.main;
+        if(colorA){
+            main.startColor = new ParticleSystem.MinMaxGradient(new Color(1.0f, 0.6f, 0.6f));
+        }
+        else{
+            main.startColor = new ParticleSystem.MinMaxGradient(new Color(0.4f, 0.4f, 0.6f));
+        }
+    }
+}
